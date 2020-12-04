15b77bd92f0ce8d3155fb2dcaf2ac644a7cf08da
Adriano Ravina <adrianoravina2g@gmail.com>
2020-12-04 12:25:28 +0100

    firebase id to customer table

--- Dump20201119/carBuilder_orders.sql
+++ Dump20201119/carBuilder_orders.sql
@@ -3,7 +3,7 @@
 -- Host: 127.0.0.1    Database: carBuilder
 -- ------------------------------------------------------
 -- Server version	8.0.17
-
+use carBuilder;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
